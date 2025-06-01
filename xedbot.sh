d1cd8f532773b7cee40c875dd57544a3f9dfb0c1
M A A Z <104779758+maazm7d@users.noreply.github.com>
2025-06-01 10:16:47 +0530

    Update xedbot.sh

--- xedbot.sh
+++ xedbot.sh
@@ -4,7 +4,6 @@ msg="*${TITLE//_/\\_}*
 ${2//\\/\\\\}
 \`\`\`
 [View Workflow Run](${GITHUB_SERVER_URL//_/\\_}/${GITHUB_REPOSITORY//_/\\_}/actions/runs/${GITHUB_RUN_ID})
-[Kernel Source](git@github.com:maazm7d/kernel_samsung_a12.git)
 *Note: Always backup working boot before flash\\.*"
 
 file="$1"
